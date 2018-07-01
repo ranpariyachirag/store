5de32ff80a76acad3469b7caa3b037b9e8d8b878
Chirag Ranpariya <ranp0004@algonquinlive.com>
2018-07-01 14:28:41 -0400

    Add files via upload

---
+++ includes/list-of-products.php
@@ -0,0 +1,52 @@
+<?php
+$products = [
+    [
+     "id" => "1", 
+     "image" => "./img/1.jpg", 
+     "type" => "chocalate", 
+     "name" => "Chocolate Fever", 
+     "price" => "2.85", 
+     "description" => "So full of chocolate it will drive you crazy."
+    ],
+    [
+     "id" => "2", 
+     "image" => "./img/2.jpg", 
+     "type" => "Cookie", 
+     "name" => "Chocalate Cookie", 
+     "price" => "3.85", 
+     "description" => "Just like delicious cup cake."
+    ],
+    [
+     "id" => "3", 
+     "image" => "./img/3.jpg", 
+     "type" => "cupcake", 
+     "name" => "Cupcake", 
+     "price" => "3.15", 
+     "description" => "Chocolate cake filled with bing cherrys and topped with whipped chocolate."
+    ],
+    [
+     "id" => "4", 
+     "image" => "./img/4.jpg", 
+     "type" => "muffin", 
+     "name" => "Muffin", 
+     "price" => "1.00", 
+     "description" => "A classic. Crisp edges and chewy middle makes these Muffin so good."
+    ],
+    [
+     "id" => "5", 
+     "image" => "./img/5.jpg", 
+     "type" => "pastry", 
+     "name" => "American Pastry", 
+     "price" => "3.50", 
+     "description" => "Sliced, unadorned apples are mounded into a Pastry crust and topped with a lattice crust."
+    ],
+    [
+     "id" => "6", 
+     "image" => "./img/6.jpg", 
+     "type" => "cake", 
+     "name" => "Cheesecake", 
+     "price" => "4.00", 
+     "description" => "A beautifully flavored, citrus-infused New York cheesecake with a graham cracker crust."
+    ]
+];
+?>
